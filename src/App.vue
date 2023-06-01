bf10141e90d4d4c9f5ca3c506ca224d394f1c1c9
Ivan Semenikhin <semenihin.ivan@gmail.com>
2023-06-01 16:03:06 +0600

    -

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useField, useForm } from 'vee-validate';
+
+const { handleSubmit } = useForm();
+const name = useField('name', undefined, { syncVModel: false });
+const { value: nameValue, errorMessage: nameError } = name;
+const onSubmit = handleSubmit(values => console.log(values));
+
+</script>
+
+<template>
+  <v-form @submit="onSubmit">
+    <v-text-field type="text" v-model.trim="nameValue" :error-messages="nameError" /> 
+  </v-form>
+
+  <hr><br>
+
+  <v-form @submit="onSubmit">
+    <v-text-field type="text" v-model.trim="nameValue" />
+  </v-form>
+</template>
